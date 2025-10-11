39e6c10dbfc13c2484959323d8be80da08b417d9
adanson-LMK <adansonsilva@gmail.com>
2025-10-10 21:14:25 -0500

    crud completo

--- app-adansonsilva-api/Data/CRUDProducto.cs
+++ app-adansonsilva-api/Data/CRUDProducto.cs
@@ -24,7 +24,10 @@ namespace app_adansonsilva_api.Data
         }
         public async Task<bool> BorrarProducto(string codigo)
         {
-            throw new NotImplementedException();
+            var bd = Conectar();
+            String sql = @"delete from tb_producto where codigo_producto = @cod";
+            int n = await bd.ExecuteAsync(sql, new { cod = codigo });
+            return n > 0;
         }
 
         public async Task<Producto> BuscarProducto(string codigo)
@@ -37,14 +40,17 @@ namespace app_adansonsilva_api.Data
 
         public async Task<bool> EditarProducto(Producto Producto)
         {
-            throw new NotImplementedException();
+            var bd = Conectar();
+            string sql = @"Update tb_producto set producto = @prod, costo = @cst, ganancia = @gnc, stock = @stk, producto_codigo_marca = @cod_mar, producto_codigo_categoria = @cod_cat where codigo_producto = @cod_prod";
+            int n = await bd.ExecuteAsync(sql, new { cod_prod = Producto.codigo_producto, prod = Producto.producto, cst = Producto.costo, gnc = Producto.ganancia, stk = Producto.stock, cod_mar = Producto.producto_codigo_marca, cod_cat = Producto.producto_codigo_categoria });
+            return n > 0;
         }
 
         public async Task<bool> RegistarProducto(Producto Producto)
         {
             var bd = Conectar();
-            string sql = @"Insert into tb_producto values @cod_prod, @prod, @cst, @gnc, @stk, @cod_mar, @cod_cat";
-            int n =  await bd.ExecuteAsync(sql, new { cod_prod = Producto.codigo_producto, prod = Producto.producto, cod_mar = Producto.producto_codigo_marca, cod_cat = Producto.producto_codigo_categoria});
+            string sql = @"insert into tb_producto values (@cod_prod, @prod, @cst, @gnc, @stk, @cod_mar, @cod_cat)";
+            int n =  await bd.ExecuteAsync(sql, new { cod_prod = Producto.codigo_producto, prod = Producto.producto, cst = Producto.costo, gnc = Producto.ganancia, stk = Producto.stock, cod_mar = Producto.producto_codigo_marca, cod_cat = Producto.producto_codigo_categoria});
             return n> 0;
         }
     }
